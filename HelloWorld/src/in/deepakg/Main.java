e35023a48db9cce0ffae6a92101106c9af4b63f1
DG <idg+git@live.in>
2012-12-09 01:48:02 +0530

    hry

---
+++ HelloWorld/src/in/deepakg/Main.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package in.deepakg;
+
+import java.io.IOException;
+
+/**
+ * @author Deepak
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("Enter something and hit enter...");
+		int s = System.in.read();
+		System.out.println(s);
+	}
+
+}
